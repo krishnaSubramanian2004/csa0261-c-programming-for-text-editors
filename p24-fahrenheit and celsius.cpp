5fd0f4d0fdea0073971657018f61108779f9339e
krishnaSubramanian2004 <122264876+krishnaSubramanian2004@users.noreply.github.com>
2023-03-29 11:50:12 +0530

    Delete p24-fahrenheit and celsius.cpp

--- p24-fahrenheit and celsius.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-   float fahrenheit, celsius;
-   printf("Enter Fahrenheit: ");
-   scanf("%f",&fahrenheit);
-   celsius = (fahrenheit - 32)*5/9;
-   printf("Celsius: %f ", celsius);
-   return 0;
-}
